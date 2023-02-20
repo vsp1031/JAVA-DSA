865e03bf459952ad413b393af0478794e8714663
Vivek Pawar <112761534+vsp1031@users.noreply.github.com>
2023-02-20 21:53:58 +0530

    BASIC PATTERNS

---
+++ pattern4.java
@@ -0,0 +1,17 @@
+package patterns;
+
+
+public class pattern4 {
+    public static void main(String args[]){
+        char ch='A';
+        for( int  i=0;i<5;i++){
+            for(int  j=0;j<i;j++){
+                System.out.print(ch);
+                ch++;
+            
+            }
+            System.out.println();
+        }
+    }
+    
+}
